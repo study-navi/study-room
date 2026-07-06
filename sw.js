ae16358a2859647a6e91c0ecc7ca850a4504b896
study-navi <baystars1899@gmail.com>
2026-07-06 23:30:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,9 @@
-const CACHE_NAME = 'study-room-stable-no-html-cache-20260706';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
+const CACHE_NAME = 'study-room-final-100';
+self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  // Always go to network. This avoids stale app files after updates.
   return;
 });
